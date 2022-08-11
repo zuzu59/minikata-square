ea64e568d5f6866cc54af33f21c906551e44d998
Christian Zufferey <christian@zufferey.com>
2022-08-11 10:21:47 +0000

    new: Création du fichier python à la sauce zuzu :-)

---
+++ square1.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+#Petit script python tout simple pour générer un carré de chiffres en lignes et en colonnes
+#zf220811.1219
+
+
+import sys
+
+
+if __name__ == '__main__':
+    print("Hello zuzu")
+
